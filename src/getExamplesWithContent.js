8be1a310ab0dec50f935b8f196009965aea19385
Luke Winship <luke.winship@gmail.com>
2021-05-26 10:07:21 +0100

    embetter

--- src/getExamplesWithContent.js
+++ src/getExamplesWithContent.js
@@ -14,7 +14,6 @@ const getExamplesWithContent = async (version) => {
         name,
         file,
         data,
-        // data: JSON.parse(fs.readFileSync(`${EXAMPLES_DIRECTORY}${file}`, { encoding: 'utf8' })),
       });
     }
   }
