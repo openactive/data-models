8625165d24fd66d87e34c5c0e69b3d8cef2f64f3
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2023-03-24 20:20:56 +0000

    feat: Make getExamplesWithContent more comprehensive (#108)

--- src/getExamplesWithContent.js
+++ src/getExamplesWithContent.js
@@ -1,19 +1,30 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const getExamples = require('./getExamples');
 
 const EXAMPLES_DIRECTORY = `${__dirname}/../versions/2.x/examples/`;
+const EXAMPLES_BASE_URL = 'https://openactive.io/data-models/versions/2.x/examples/';
 
-const getExamplesWithContent = async (version) => {
+
+const getExamplesWithContent = (version, fetcherForRemoteExamplesSync) => {
   const examples = getExamples(version);
+  const flattenedExamples = examples.flatMap(x => x.exampleList.map(example => ({ category: x.name, ...example })));
   const output = [];
-  for (const { file, name } of examples.flatMap(x => x.exampleList)) {
-    // Only include local examples
-    if (file.indexOf('http') !== 0) {
-      const data = JSON.parse(await fs.readFile(`${EXAMPLES_DIRECTORY}${file}`, { encoding: 'utf8' }));
+  for (const example of flattenedExamples) {
+    if (example.file.indexOf('http') !== 0) {
+      // Local example file
+      const data = JSON.parse(fs.readFileSync(`${EXAMPLES_DIRECTORY}${example.file}`, { encoding: 'utf8' }));
       output.push({
-        name,
-        file,
         data,
+        url: `${EXAMPLES_BASE_URL}${example.file}`,
+        ...example,
+      });
+    } else if (fetcherForRemoteExamplesSync) {
+      // Remote example file
+      const data = fetcherForRemoteExamplesSync(`${example.file}`);
+      output.push({
+        data,
+        url: example.file,
+        ...example,
       });
     }
   }
