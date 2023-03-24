494569f5e4a4ff6c386c61bcece0a5b00b3063bf
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2023-03-24 20:08:47 +0000

    feat: Make getExamplesWithContent more comprehensive

--- src/getExamplesWithContent-spec.js
+++ src/getExamplesWithContent-spec.js
@@ -6,14 +6,29 @@ describe('getExamplesWithContent', () => {
   });
 
   it('should return valid examples as JSON', async () => {
-    const examples = await getExamplesWithContent('2.0');
+    const examples = getExamplesWithContent('2.0');
     expect(typeof examples).toBe('object');
     expect(examples instanceof Array).toBe(true);
   });
 
   it('should return valid examples as JSON with a version alias', async () => {
-    const examples = await getExamplesWithContent('latest');
+    const examples = getExamplesWithContent('latest');
     expect(typeof examples).toBe('object');
     expect(examples instanceof Array).toBe(true);
+  });
+
+  it('should use remote fetcher for remote examples', async () => {
+    const mockFetcher = x => `Fetcher:${x}`;
+
+    const examples = getExamplesWithContent('latest', mockFetcher);
+    expect(typeof examples).toBe('object');
+    expect(examples instanceof Array).toBe(true);
+
+    const remoteExamples = examples.filter(x => x.file.indexOf('http') === 0);
+    expect(remoteExamples).toBeNonEmptyArray();
+
+    for (const { file, data } of remoteExamples) {
+      expect(data).toBe(mockFetcher(file));
+    }
   });
 });
