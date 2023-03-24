cb15f35a78a0a94fef28b48ac894760cb9446a1a
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2023-03-24 20:17:57 +0000

    Fix sync error

--- src/getExamplesWithContent-spec.js
+++ src/getExamplesWithContent-spec.js
@@ -2,7 +2,7 @@ const getExamplesWithContent = require('./getExamplesWithContent');
 
 describe('getExamplesWithContent', () => {
   it('should throw if passed an invalid spec version', async () => {
-    expectAsync(getExamplesWithContent('0.0')).toBeRejected();
+    expect(getExamplesWithContent('0.0')).toThrow();
   });
 
   it('should return valid examples as JSON', async () => {
